15bc59da8a1d7eac44b2fbb61786cb933e99c2e5
luciazarate <veronicazarate7826@gmail.com>
2023-03-17 14:59:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BackEnd_lvz
+ repositorio del BackEND de mi portfolio ARGENTINA PROGRAMA 2023
---
+++ src/main/java/com/portfolio/mds/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+
+package com.portfolio.mds.Repository;
+
+import com.portfolio.mds.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
