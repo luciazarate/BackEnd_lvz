bba8a64617bf4d47b6d83aed0d7d30e933e277ac
luciazarate <veronicazarate7826@gmail.com>
2023-03-23 15:00:53 -0300

    se agregan target y dockerfile

---
+++ src/main/java/com/portfolio/lvz/Security/Enums/RolNombre.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.lvz.Security.Enums;
+
+/**
+ *
+ * @author lucia veronica zarate
+ */
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
+    
+}
---
+++ src/main/java/com/portfolio/lvz/lvzApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.lvz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class lvzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(lvzApplication.class, args);
+	}
+
+}
